26097adae48c8fab08c0b2fd0094663cec6811c2
George <george.ursachi7@gmail.com>
2020-09-05 19:24:28 +0200

    Node class basic setup

---
+++ DBankExercise/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBankExercise
+{
+    class Node
+    {
+        private int value;
+        private Node child1;
+        private Node child2;
+
+        public Node(int value, Node child1, Node child2)
+        {
+            this.value = value;
+            this.child1 = child1;
+            this.child2 = child2;
+        }
+
+        //Getters and Setters
+        public int Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public Node Child1
+        {
+            get { return child1; }
+            set { child1 = value}
+        }
+
+        public Node Child2
+        {
+            get { return child2; }
+            set { child2 = value; }
+        }
+    }
+}
---
+++ DBankExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBankExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
